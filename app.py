373d65837860693f2294bd5d8ebcfd71ceb814db
Dennis <denchyk228@gmail.com>
2023-05-25 16:31:52 +0200

    Added data.txt file checking

--- app.py
+++ app.py
@@ -1,24 +1,32 @@
 from flask import Flask, jsonify, request, render_template
 from random import randint
 import json
+import os
 app = Flask(__name__)
+# flask --app app run --debug
+
+
+def check_file():
+    if os.path.exists('data.txt'):
+        try:
+            with open('data.txt') as file:
+                content = file.read()
+                json.loads(content)
+
+        except ValueError:
+            with open('data.txt', 'w') as file:
+                file.write('{}')
+    else:
+        with open('data.txt', 'w') as file:
+            file.write('{}')
+
+
+check_file()
 
 
 @app.route('/')
 def get_main_page():
     return render_template("index.html")
-
-
-@app.route('/gamestate', methods=['GET'])
-def get_game_state():
-    # Retrieve the current game state
-    # Implement this function to read the game state from a file
-    # game_state = {"tiles": [1, 2, 3, 1, 3]}
-    game_state = {"tiles": [[randint(0, 3), randint(0, 3)], [
-        randint(0, 3), randint(0, 3)]]}
-
-    # Return the game state as JSON
-    return jsonify(game_state)
 
 
 @app.route('/get-user', methods=['GET'])
